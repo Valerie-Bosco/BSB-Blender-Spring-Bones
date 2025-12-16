727ebb84663076da37fac02c263167a2f0c8c7df
Valery-AA <valeryatart@gmail.com>
2025-12-16 06:24:21 +0100

    added pycharm #type:ignore

--- BSB-Blender-Spring-Bones/BSB_Properties.py
+++ BSB-Blender-Spring-Bones/BSB_Properties.py
@@ -1,14 +1,15 @@
 import bpy
+import mathutils
 
 
 class BSB_PG_ObjectProperties(bpy.types.PropertyGroup):
     b_collider: bpy.props.BoolProperty(name="", default=False)  # type:ignore
-    collider_radius: bpy.props.FloatProperty(
+    collider_radius: bpy.props.FloatProperty(# type:ignore
         name="Collider Distance",
         default=0.5,
         description="Minimum distance to handle collision between the spring and collider bones"
     )  # type:ignore
-    collider_repulsion_force: bpy.props.FloatProperty(
+    collider_repulsion_force: bpy.props.FloatProperty(# type:ignore
         name="Collider Force",
         default=1.0,
         description="Amount of repulsion force when colliding"
@@ -16,42 +17,56 @@ class BSB_PG_ObjectProperties(bpy.types.PropertyGroup):
 
 
 class BSB_PG_PoseBoneProperties(bpy.types.PropertyGroup):
-    global_influence: bpy.props.FloatProperty(
+    global_influence: bpy.props.FloatProperty(# type:ignore
         name="Influence",
         default=1.0,
         min=0.0, max=1.0,
         description="Global influence of spring motion"
     )  # type:ignore
 
-    b_enable_spring: bpy.props.BoolProperty(
+    b_enable_spring: bpy.props.BoolProperty(# type:ignore
         name="Enabled",
         default=False,
         description="Enable spring effect on this bone"
     )  # type:ignore
-    b_enable_bone_rotation: bpy.props.BoolProperty(
+    b_enable_bone_rotation: bpy.props.BoolProperty(# type:ignore
         name="Bone Rotation",
         default=False
     )  # type:ignore
-    spring_stiffness: bpy.props.FloatProperty(
+    spring_stiffness: bpy.props.FloatProperty(# type:ignore
         name="Stiffness",
         default=0.5,
         min=0.01, max=1.0,
         description="Bouncy/elasticity value, higher values lead to more bounciness"
     )  # type:ignore
-    spring_dampening_force: bpy.props.FloatProperty(
+    spring_dampening_force: bpy.props.FloatProperty(# type:ignore
         name="Damp",
         default=0.7,
         min=0.0, max=10.0,
         description="Speed/damping force applied to the bone to go back to it initial position"
     )  # type:ignore
-    spring_gravity: bpy.props.FloatProperty(
+    spring_gravity: bpy.props.FloatProperty( # type:ignore
         name="Gravity",
         description="Additional vertical force to simulate gravity",
         default=-9.81,
         min=-100.0, max=100.0
     )  # type:ignore
 
-    spring_axis_lock: bpy.props.EnumProperty(
+    spring_gravity_vector: bpy.props.FloatVectorProperty( # type:ignore
+        name="Gravity Direction",
+        description="The direction of gravity",
+        default=(0, 0, 1),
+        min=-1, max=1
+    )  # type:ignore
+
+    spring_tail_mass: bpy.props.FloatProperty(# type:ignore
+        name="Mass",
+        description="Mass in grams",
+        default=50.0,
+        min=10.0
+    )  # type:ignore
+
+    spring_axis_lock: bpy.props.EnumProperty(# type:ignore
         items=(('NONE', 'None', ""),
                ('+X', '+X', ''),
                ('-X', '-X', ''),
@@ -62,11 +77,11 @@ class BSB_PG_PoseBoneProperties(bpy.types.PropertyGroup):
         default="NONE"
     )  # type:ignore
 
-    b_enable_as_collider: bpy.props.BoolProperty(
+    b_enable_as_collider: bpy.props.BoolProperty(# type:ignore
         name="Collider",
         default=False,
         description="Enable this bone as collider")  # type:ignore
-    b_should_collide: bpy.props.BoolProperty(
+    b_should_collide: bpy.props.BoolProperty(# type:ignore
         name="should collide",
         default=True,
         description="The bone will collide with other colliders"
@@ -75,55 +90,80 @@ class BSB_PG_PoseBoneProperties(bpy.types.PropertyGroup):
 
 class BSB_PG_SpringBone(bpy.types.PropertyGroup):
     armature: bpy.props.PointerProperty(type=bpy.types.Object)  # type:ignore
-    last_location: bpy.props.FloatVectorProperty(
-        name="Loc",
+
+    # region INTER MODE ONLY
+    last_head_location: bpy.props.FloatVectorProperty(# type:ignore
+        name="",
         subtype='DIRECTION',
         default=(0, 0, 0),
         size=3
     )  # type:ignore
-    speed: bpy.props.FloatVectorProperty(
+    last_tail_location: bpy.props.FloatVectorProperty(# type:ignore
         name="Speed",
         subtype='DIRECTION',
         default=(0, 0, 0),
         size=3
     )  # type:ignore
-    distance: bpy.props.FloatProperty(
+    # endregion
+
+    # region ANIM MODE ONLY
+
+    # frame:wco
+    head_frame_wco_pairing: dict[int,mathutils.Vector]
+    tail_frame_wco_pairing: dict[int,mathutils.Vector]
+
+    # endregion
+
+    head_speed: bpy.props.FloatVectorProperty(# type:ignore
+        name="Speed",
+        subtype='DIRECTION',
+        default=(0, 0, 0),
+        size=3
+    )  # type:ignore
+    tail_speed: bpy.props.FloatVectorProperty(# type:ignore
+        name="Speed",
+        subtype='DIRECTION',
+        default=(0, 0, 0),
+        size=3
+    )  # type:ignore
+
+    distance: bpy.props.FloatProperty(# type:ignore
         name="distance",
         default=1.0
     )  # type:ignore
-    target_offset: bpy.props.FloatVectorProperty(
+    target_offset: bpy.props.FloatVectorProperty(# type:ignore
         name="TargetLoc",
         subtype='DIRECTION',
         default=(0, 0, 0),
         size=3
     )  # type:ignore
-    bone_collider: bpy.props.BoolProperty(
+    bone_collider: bpy.props.BoolProperty(# type:ignore
         name="Bone collider",
         default=False
     )  # type:ignore
-    b_is_bone_colliding: bpy.props.BoolProperty(
+    b_is_bone_colliding: bpy.props.BoolProperty(# type:ignore
         name="Bone colliding",
         default=True
     )  # type:ignore
-    collider_radius: bpy.props.FloatProperty(
+    collider_radius: bpy.props.FloatProperty(# type:ignore
         name="Bone collider distance",
         default=0.5
     )  # type:ignore
-    sb_collider_force: bpy.props.FloatProperty(
+    sb_collider_force: bpy.props.FloatProperty(# type:ignore
         name="Bone collider force",
         default=1.0
     )  # type:ignore
 
-    b_enable_as_collider: bpy.props.BoolProperty(
+    b_enable_as_collider: bpy.props.BoolProperty(# type:ignore
         name="Collider",
         default=False,
         description="Enable this bone as collider"
     )  # type:ignore
-    collider_radius: bpy.props.FloatProperty(
+    collider_radius: bpy.props.FloatProperty(# type:ignore
         name="Collider Distance",
         default=0.5,
         description="Minimum distance to handle collision between the spring and collider bones")  # type:ignore
-    collider_repulsion_force: bpy.props.FloatProperty(
+    collider_repulsion_force: bpy.props.FloatProperty(# type:ignore
         name="Collider Force",
         default=1.0,
         description="Amount of repulsion force when colliding"
@@ -135,31 +175,31 @@ class BSB_PG_MeshColliders(bpy.types.PropertyGroup):
 
 
 class BSB_PG_SceneProperties(bpy.types.PropertyGroup):
-    simulation_scene: bpy.props.PointerProperty(
+    simulation_scene: bpy.props.PointerProperty(# type:ignore
         type=bpy.types.Scene
     )  # type:ignore
 
-    b_global_spring: bpy.props.BoolProperty(
+    b_global_spring: bpy.props.BoolProperty(# type:ignore
         name="Enable spring",
         default=False
     )  # type:ignore
-    b_global_sim_only_on_frame_change: bpy.props.BoolProperty(
+    b_global_sim_only_on_frame_change: bpy.props.BoolProperty(# type:ignore
         name="Enable Spring",
         description="Enable Spring on frame change only",
         default=False
     )  # type:ignore
-    b_show_colliders: bpy.props.BoolProperty(
+    b_show_colliders: bpy.props.BoolProperty(# type:ignore
         name="Show Colliders",
         description="Show active colliders names",
         default=False
     )  # type:ignore
 
-    b_use_scene_framerate: bpy.props.BoolProperty(
+    b_use_scene_framerate: bpy.props.BoolProperty(# type:ignore
         name="",
         default=True
     )  # type:ignore
 
-    custom_framerate: bpy.props.FloatProperty(
+    custom_framerate: bpy.props.FloatProperty(# type:ignore
         name="",
         default=24.0,
         min=1.0,
